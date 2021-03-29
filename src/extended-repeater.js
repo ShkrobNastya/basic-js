a64ea6e049428042a06cac9fba7db868a6ea7302
ShkrobNastya <60944202+ShkrobNastya@users.noreply.github.com>
2021-03-29 23:20:26 +0300

    feat: add code4

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+  let res = [];
+  let allSepArr = [];
+  let allSep = '';
+
+  let key = options.additionRepeatTimes;
+  if ((!key) && (options.addition)) {
+    key = 1;
+  }
+  let addSep = options.additionSeparator;
+  if (!addSep) {
+    addSep = '|';
+  }
+      for (let i = 0; i < key; i++) {
+        allSepArr.push(String(options.addition));
+        allSepArr.push(addSep);
+      }
+      allSepArr.pop();
+
+    allSep = allSepArr.join('');
+
+     key = options.repeatTimes;
+     if (!key) {
+       return str + options.addition;
+     }
+      for (let i = 0; i < key; i++) {
+        res.push(str+allSep);
+    }
+
+    key = options.separator;
+    if(!key) {
+      key = "+";
+    }
+    res = res.join(key);
+
+  return res;
 };
   
