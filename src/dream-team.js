14f31db345f05320fee4071931079f2a8f37184e
ShkrobNastya <60944202+ShkrobNastya@users.noreply.github.com>
2021-03-29 23:19:44 +0300

    feat: add code3

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members ) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let arr=[];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i])==='string') {
+      let a=members[i].split('');
+      let j = 0;
+      while (a[j] == ' ') {
+        j++;
+      }
+      arr.push(a[j].toUpperCase());
+       arr.sort();
+    }
+  }
+
+  return arr.join('');
 };
